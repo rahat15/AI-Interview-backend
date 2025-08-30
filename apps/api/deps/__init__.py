68c1b4d44a55774d5b51582a1b14d0fd0b2b8003
Rahat <rbhatia@ewu.edu>
2025-08-29 23:46:37 -0700

    tt

--- apps/api/deps/__init__.py
+++ apps/api/deps/__init__.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
 
-from core.config import settings  # wherever your DB settings live
+# from core.config import settings  # wherever your DB settings live
 
-engine = create_engine(settings.database_url, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# engine = create_engine(settings.database_url, pool_pre_ping=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_engine():
-    return engine
+# def get_engine():
+#     return engine
